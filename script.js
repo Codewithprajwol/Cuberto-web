3af51bde7a2840e453f7edf2103337aa78dcde00
prajwol shrestha <shresthaprajwol161@gmail.com>
2024-02-04 15:57:58 +0545

    side way animation is completed

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 Shery.mouseFollower();
+Shery.makeMagnet(".magnet");
+Shery.hoverWithMediaCircle(".hoverani",{videos:["/0.mp4","/2.mp4","/3.mp4"]})
+// Shery.imageEffect(".images", {
+//     style: 5,
+//     slideStyle: (setScroll) => {
+//       window.addEventListener("scroll", () => {
+//         setScroll(window.scrollY / innerHeight); //Updating the scroll
+//       });
+//     },
+//   });
