2f88a66a22c20e9d9cae91b2b37af56576da0141
prajwol shrestha <shresthaprajwol161@gmail.com>
2024-02-05 18:27:21 +0545

    cloning cuberto web with major animations

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 Shery.mouseFollower();
 Shery.makeMagnet(".magnet");
 Shery.hoverWithMediaCircle(".hoverani",{videos:["/0.mp4","/2.mp4","/3.mp4"]})
-// Shery.imageEffect(".images", {
-//     style: 5,
-//     slideStyle: (setScroll) => {
-//       window.addEventListener("scroll", () => {
-//         setScroll(window.scrollY / innerHeight); //Updating the scroll
-//       });
-//     },
-//   });
+gsap.to(".fleftelem",{
+    scrollTrigger:{
+        trigger:"#featureimages",
+        start:"top top",
+        end:"end end",
+        endTrigger:".last",
+        pin:true,
+        scrub:1,
+        // markers:true,
+    },
+   y:"-300%",
+   ease:Power1
+})
+let prajwol=document.querySelectorAll(".fleftelem");
+Shery.imageEffect(".fimages", {
+    style: 2,
+    // config: {onMouse:{value:1}},
+    slideStyle: (setScroll) => {
+      prajwol.forEach(function(section,index){
+        ScrollTrigger.create({
+            trigger:section,
+            start:"top top",
+            scrub:1,
+            onUpdate:function(prog){
+               setScroll(prog.progress+index)
+            },
+        });
+      });
+    },
+  });
